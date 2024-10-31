a9623d0bb4d2ccc2f4e929b5574ba25f00675e8a
Zahir Hidalgo <pedrozahir912@gmail.com>
2024-10-31 00:26:45 -0300

    Primer commit: Portafolio inicial

---
+++ java.js
@@ -0,0 +1,9 @@
+
+// Funcionalidad para realizar una transición suave entre secciones
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const sectionId = this.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
